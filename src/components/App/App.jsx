711bec802a1792af364c929c0d43ecf5c07636e7
Vadim <hellovados@gmail.com>
2022-04-17 15:42:37 +0300

    затестил bootstrap

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { Row, Button, Col } from "react-bootstrap";
 
 import { fetchSinglePhoto } from "../../redux/actions/singlePhotoActions";
 
@@ -8,7 +9,21 @@ const App = () => {
   const dispatch = useDispatch();
   const photo = useSelector((state) => state.singlePhoto);
 
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Row className="mx-0">
+        <Button as={Col} variant="primary">
+          Button #1
+        </Button>
+        <Button as={Col} variant="secondary" className="mx-2">
+          Button #2
+        </Button>
+        <Button as={Col} variant="success">
+          Button #3
+        </Button>
+      </Row>
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.scss";
 import App from "./components/App/App";
